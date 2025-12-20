79fd946d37e2fcf1c44a969b4e3d849fb93a5420
mayurharinkhede800 <mayurharinkhede800@gmail.com>
2025-12-20 23:48:16 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,16 @@
+-- Create DataBase
+use master;
+-- Creating DataBase
+create database DataWareHouse
 
+-- use Database
+use DataWareHouse
+
+-- create Schemas
+
+create schema bronze
+go
+create schema silver
+go
+create schema gold	
+go
